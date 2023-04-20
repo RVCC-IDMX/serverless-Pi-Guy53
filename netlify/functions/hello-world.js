77c3ea6a589976dbb06dd08e4bd49aaa0b72a6d7
UsrnGamesInc <71029215+Pi-Guy53@users.noreply.github.com>
2023-04-20 16:41:20 -0400

    added colorful terminal output

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,4 +1,10 @@
+const chalk = require('chalk');
+const { DateTime } = require('luxon');
+
 exports.handler = async function () {
+    const date = DateTime.now();
+    console.log(chalk.bgWhite(`${date}: Hello world Serverless function`));
+
     return {
         statusCode: 200,
         body: JSON.stringify({
--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,3 +1,6 @@
+const chalk = require('chalk');
+const { DateTime } = require('luxon');
+
 // mod.cjs
 // eslint-disable-next-line no-shadow
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
@@ -6,10 +9,19 @@ exports.handler = async function (event, context) {
     const eventBody = JSON.parse(event.body);
     const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region;
 
+    const date = DateTime.now();
+    const color = eventBody.region === 'kanto' ? chalk.blue : chalk.green;
+    const timeColor = chalk.bgWhite;
+
+    console.log(timeColor(`Fetched at: ${date}`));
+    console.log(color(`\tAcessed Region: ${eventBody.region}`));
+
     // console.log(event);
 
     const response = await fetch(POKE_API);
     const data = await response.json();
+    console.log(color(`\tTotal Entry Count: ${data.pokemon_entries.length}`));
+
     return {
         statusCode: 200,
         body: JSON.stringify({
